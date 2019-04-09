95205cb54da3751f5fe616130d2f173fea785219
wana_li <744279232@qq.com>
2019-04-09 17:51:25 +0800

    修改

--- homework1109/Package.cpp
+++ homework1109/Package.cpp
@@ -1,37 +1,147 @@
 ﻿#include "Package.h"
 
-Package::Package()
+Package::Package(const Personal_Data &MySender,
+                 const Personal_Data &MyAddressee,
+                 double               weight,
+                 double               unitPrice):
+                 Sender(MySender),Addressee(MyAddressee),Weight(weight),Unit_Price(unitPrice)
 {
 
-    Sender      = "";
-    Addressee   = "";
-    City        = "";
-    State       = "";
-    Postal_Code = "";
-    Weight      = 1 ;
-    Unit_Price  = 1 ;
 }
 
-double Package::calculateCost() const
+double Package::calculateCost()
 {
     return Weight*Unit_Price;
 }
 
-TwoDayPackage:: TwoDayPackage(double price)
-
+Personal_Data Package::getMyAddressee() const
 {
-    Day_Price = price;
+    return Addressee;
 }
-double TwoDayPackage::calculateCost()const
-{
-    return  Day_Price+Weight*Unit_Price;
-}
- OverNightPackage::OverNightPackage(double price)
- {
-     Unit_Price = Unit_Price + price/Weight;
- }
 
- double OverNightPackage::calculateCost()
- {
-     return  Weight*Unit_Price;
- }
+void Package::setAddressee(const Personal_Data &MyAddressee)
+{
+    Addressee = MyAddressee;
+}
+
+Personal_Data Package::getMySender() const
+{
+    return Sender;
+}
+
+void Package::setSender(const Personal_Data &MySender)
+{
+    Sender = MySender;
+}
+
+double Package::getMyWeight() const
+{
+    return Weight;
+}
+
+void Package::setWeight(double MyWeight)
+{
+    Weight = ((MyWeight < 0) ? 0 : MyWeight);
+}
+
+double Package::getMyUnitPrice() const
+{
+    return Unit_Price;
+}
+
+void Package::setUnitPrice(double MyUnitPrice)
+{
+    Unit_Price = ((MyUnitPrice < 0) ? 0 : MyUnitPrice);
+}
+TwoDayPackage::TwoDayPackage(const Personal_Data &MySender,
+                             const Personal_Data &MyAddressee,
+                             double               weight,
+                             double               unitPrice,
+                             double               DayCost)
+    :Package(MySender,MyAddressee, weight,unitPrice)
+{
+    setDayPrice(DayCost);
+
+}
+
+
+
+double TwoDayPackage::calculateCost()
+{
+    double newUnitCost = getMyUnitPrice() + Day_Price;
+    return newUnitCost * getMyWeight();
+}
+
+
+double TwoDayPackage::getMyDayPrice() const
+{
+    return Day_Price;
+}
+void   TwoDayPackage::setDayPrice(double MyDayPrice)
+{
+    Day_Price = MyDayPrice;
+}
+
+OverNightPackage::OverNightPackage(const Personal_Data &MySender,
+                                   const Personal_Data &MyAddress,
+                                   double               weight,
+                                   double               unitPrice,
+                                   double               NightCost)
+    :Package (MySender,MyAddress,weight,unitPrice)
+{
+    setNightPrice(NightCost);
+}
+
+
+void OverNightPackage::setNightPrice(double MyNightPrice)
+{
+    Night_Price = MyNightPrice;
+}
+
+double OverNightPackage::getMyNightPrice()const
+{
+    return  Night_Price;
+}
+
+double OverNightPackage::calculataCost()
+{
+
+    double newUnitCost = Package::calculateCost() + Night_Price;
+    return newUnitCost * Package::getMyWeight();
+}
+//double OvernightPackage::overnightCost() const
+//{
+//    return mOvernightCost;
+//}
+
+//void OvernightPackage::setOvernightCost(double overnightCost)
+//{
+//    mOvernightCost = ((overnightCost < 0) ? 0 : overnightCost);
+//}
+
+//TwoDayPackage::TwoDayPackage(const PersonContact &addresser,
+//                             const PersonContact &receiver,
+//                             double               weight,
+//                             double               unitCost,
+//                             double               twoDayCost)
+//    : Package(addresser, receiver, weight, unitCost)
+//{
+//    setTwoDayCost(twoDayCost);
+//}
+
+//double TwoDayPackage::calculateCost()
+//{
+//    // double base = unitCost() * weight();
+//    double base = Package::calculateCost();
+//    return base + mTwoDayCost;
+//}
+
+//double TwoDayPackage::twoDayCost() const
+//{
+//    return mTwoDayCost;
+//}
+
+//void TwoDayPackage::setTwoDayCost(double twoDayCost)
+//{
+//    mTwoDayCost = ((twoDayCost < 0) ? 0 : twoDayCost);
+//}
--- homework1109/main.cpp
+++ homework1109/main.cpp
@@ -5,15 +5,31 @@ using namespace std;
 
 int main()
 {
-    Package Pack1;
-    TwoDayPackage Pack2(3);
-    OverNightPackage Pack3(5);
-    double Cost1 = Pack1.calculateCost();
-    double Cost2 = Pack2.calculateCost();
-    double Cost3 = Pack3.calculateCost();
-    cout<<"Cost1:"<<Cost1<<endl;
-    cout<<"Cost2:"<<Cost2<<endl;
-    cout<<"Cost3:"<<Cost3<<endl;
 
+    cout << "Examples!" << endl;
+
+    Personal_Data sender = {"a", "ShanDongKeJiDaXue", "QingDao", "ShanDong", 266590};
+    Personal_Data reseiver  = {"b", "ShanDongKeJiDaXue", "QingDao", "ShanDong", 266590};
+    Package       a(sender, reseiver, 100, 20);
+    cout << "weigth:" << a.getMyWeight() << endl;
+    cout << "unit cost:" << a.getMyUnitPrice() << endl;
+    cout << "sum cost:" << a.calculateCost() << endl;
+    cout << "***************************************" << endl;
+
+
+    TwoDayPackage e(sender, reseiver, 100, 20, 7.4);
+    cout << "weigth:" << e.getMyWeight() << endl;
+    cout << "unit cost:" << e.getMyUnitPrice() << endl;
+    cout << "two day cost:" << e.getMyDayPrice() << endl;
+    cout << "sum cost:" << e.calculateCost() << endl;
+    cout << "***************************************" << endl;
+
+
+    OverNightPackage f1(sender, reseiver,100, 20, -7.4);
+    cout << "weigth:" << f1.getMyWeight() << endl;
+    cout << "unit cost:" << f1.getMyUnitPrice() << endl;
+    cout << "overnight cost:" << f1.getMyNightPrice() << endl;
+    cout << "sum cost:" << f1.calculateCost() << endl;
+    return 0;
 
 }
